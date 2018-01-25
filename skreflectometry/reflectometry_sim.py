c8c5b9afc44042cc60e5e3bc5cd660f656e78f6c
Duarte Nina <duarte.r.nina@gmail.com>
2018-01-25 12:47:53 +0000

    Fixed bugs with beat.

--- skreflectometry/reflectometry_sim.py
+++ skreflectometry/reflectometry_sim.py
@@ -6,13 +6,13 @@ from scipy.constants import speed_of_light
 from scipy.signal import spectrogram
 
 
-def phase_delay(freq_samp, radius_arr, refractive_mat,
+def phase_delay(freq_probing, radius_arr, refractive_mat,
                 refraction_epsilon=1e-6, reflect_at_wall=True, method='trapz'):
     """
     TODO
     Parameters
     ----------
-    freq_samp
+    freq_probing
     radius_arr
     refractive_mat
     refraction_epsilon
@@ -39,10 +39,10 @@ def phase_delay(freq_samp, radius_arr, refractive_mat,
     reflect_at_0_measured_ind = (reflect_pos_ind == 0)
     reflect_at_0_fake_ind = reflect_at_0_ind ^ reflect_at_0_measured_ind
 
-    if reflect_at_wall:
-        reflect_pos_ind[reflect_at_0_fake_ind] = refractive_mat.shape[1]  # - 1
+    if reflect_at_wall:  # TODO
+        reflect_pos_ind[reflect_at_0_fake_ind] = - 1
     else:
-        reflect_pos_ind[reflect_at_0_fake_ind] = -1
+        reflect_pos_ind[reflect_at_0_fake_ind] = 0
 
     if method == 'trapz':
         integral_func = np.trapz
@@ -52,23 +52,23 @@ def phase_delay(freq_samp, radius_arr, refractive_mat,
         raise ValueError("Parameter 'method' must be 'trapz' or 'simps'")
 
     refract_mat_temp = np.copy(refractive_mat)
-    for freq_ind in range(len(freq_samp)):
+    for freq_ind in range(len(freq_probing)):
         refract_mat_temp[freq_ind, reflect_pos_ind[freq_ind]:-1] = 0
 
     refract_int = integral_func(refract_mat_temp, radius_arr, axis=1)
 
-    phase_diff = 2 * 2 * np.pi * freq_samp / speed_of_light * refract_int
+    phase_diff = 2 * 2 * np.pi * freq_probing / speed_of_light * refract_int
     phase_diff -= np.pi / 2
 
     return phase_diff
 
 
-def time_delay(freq_samp, phase_delay_arr):
+def time_delay(freq_probing, phase_delay_arr):
     """
     TODO
     Parameters
     ----------
-    freq_samp
+    freq_probing
     phase_delay_arr
 
     Returns
@@ -77,39 +77,53 @@ def time_delay(freq_samp, phase_delay_arr):
     """
 
     phase_diff = np.gradient(phase_delay_arr)
-    omega_diff = np.gradient(2 * np.pi * freq_samp)
+    omega_diff = np.gradient(2 * np.pi * freq_probing)
 
     return phase_diff / omega_diff
 
 
-def beat_spectrogram(freq_samp, time_delay_arr, fs=1.0,
-                     nperseg=136, nfft=2048, noverlap=128):
+# def beat_spectrogram(beat, fs=1.0,
+#                      nperseg=136, nfft=2048, noverlap=128):
+#     """
+#     TODO
+#     Parameters
+#     ----------
+#     freq_samp
+#     time_delay_arr
+#     fs
+#     nperseg
+#     nfft
+#     noverlap
+#
+#     Returns
+#     -------
+#
+#     """
+#
+#     freqs, times, spectrum = \
+#         spectrogram(beat, fs=fs,
+#                     nperseg=nperseg, nfft=nfft, noverlap=noverlap)
+#
+#     return freqs, times, spectrum
+
+
+def beat_signal(freq_probing, time_delay_arr):
     """
     TODO
     Parameters
     ----------
-    freq_samp
+    freq_probing
     time_delay_arr
-    fs
-    nperseg
-    nfft
-    noverlap
 
     Returns
     -------
 
     """
 
-    beat = np.sin(np.cumsum(time_delay_arr * np.gradient(freq_samp)))
-
-    freqs, times, spectrum = \
-        spectrogram(beat, fs=fs,
-                    nperseg=nperseg, nfft=nfft, noverlap=noverlap)
-
-    return freqs, times, spectrum
+    return np.cos(np.cumsum(time_delay_arr * np.gradient(2 * np.pi * freq_probing)))
 
 
-def beat_signal(freqs, spectrum):
+def beat_maximums(freqs, spectrum):
     """
     TODO
     Parameters
